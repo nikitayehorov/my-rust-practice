3ebcc1fd124001e2f1fa503b99bfedfe98b232b8
nikitayehorov <nikita.yehorov@e-u.edu.ua>
2026-05-10 15:17:24 +0300

    Practical Work #13: Implementation of Divisible Sum Pairs

--- src/hackerrank/mod.rs
+++ src/hackerrank/mod.rs
@@ -1,1 +1,1 @@
-pub mod task0; pub mod task3; pub mod task4; pub mod task5; pub mod task6; pub mod task07; pub mod task08; pub mod task09; pub mod task10; pub mod task11; pub mod task12;
+pub mod task0; pub mod task3; pub mod task4; pub mod task5; pub mod task6; pub mod task07; pub mod task08; pub mod task09; pub mod task10; pub mod task11; pub mod task12; pub mod task13;
